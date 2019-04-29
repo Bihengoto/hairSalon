9e9645609da90bf0003dd842992ff7a7a46fae36
Bihengoto <isaacbihengoto@gmail.com>
2019-04-29 03:59:02 +0300

    added and declared the various fields in the client file and made minor changes to the DB file

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -3,17 +3,17 @@ import org.sql2o.*;
 import java.util.List;
 
 public class Client {
-    private int id;
-    private String name, phone, gender, email;
-    private int stylist_id;
-
-    public Client(String name, String phone, String gender, String email, int stylist_id) {
-        this.name = name;
-        this.phone = phone;
-        this.gender = gender;
-        this.email = email;
-        this.stylist_id = stylist_id;
-    }
+//    private int id;
+//    private String name, phone, gender, email;
+//    private int stylist_id;
+//
+//    public Client(String name, String phone, String gender, String email, int stylist_id) {
+//        this.name = name;
+//        this.phone = phone;
+//        this.gender = gender;
+//        this.email = email;
+//        this.stylist_id = stylist_id;
+//    }
 
     public int getId() {
         return id;
@@ -81,26 +81,4 @@ public class Client {
         }
     }
 
-//    public void delete() {
-//        try(Connection con = DB.sql2o.open()) {
-//            String sql = "DELETE FROM clients WHERE id = :id;";
-//            con.createQuery(sql)
-//                    .addParameter("id", id)
-//                    .executeUpdate();
-//        }
-//    }
-//
-//    public void update(String name, String phone, String email, String gender, int stylist_id) {
-//        try(Connection con = DB.sql2o.open()) {
-//            String sql = "UPDATE clients SET name = :name, phone = :phone, email = :email, stylist_id = :stylist_id  WHERE id = :id";
-//            con.createQuery(sql)
-//                    .addParameter("id", id)
-//                    .addParameter("name",name )
-//                    .addParameter("phone",phone )
-//                    .addParameter("gender",gender)
-//                    .addParameter("email",email )
-//                    .addParameter("stylist_id", stylist_id)
-//                    .executeUpdate();
-//        }
-//    }
 }
