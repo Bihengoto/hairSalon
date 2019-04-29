429e56e0690f684de79330a690db4248cddb4f4f
Bihengoto <isaacbihengoto@gmail.com>
2019-04-29 04:34:08 +0300

    added all the tests for the stylists in its respective page and fixed a few bugs in the client file

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -39,46 +39,46 @@ public class Client {
         return stylist_id;
     }
 
-//    public static List<Client> all() {
-//        String sql = "SELECT * FROM clients";
-//        try(Connection con = DB.sql2o.open()) {
-//            return con.createQuery(sql).executeAndFetch(Client.class);
-//        }
-//    }
-//
-//    public void save() {
-//        try(Connection con = DB.sql2o.open())  {
-//            String sql = "INSERT INTO clients (name, phone, gender, email, stylist_id) VALUES (:name, :phone, :gender, :email, :stylist_id)";
-//            this.id = (int) con.createQuery(sql, true)
-//                    .addParameter("name", this.name)
-//                    .addParameter("phone",this.phone)
-//                    .addParameter("gender", this.gender)
-//                    .addParameter("email", this.email)
-//                    .addParameter("stylist_id", this.stylist_id)
-//                    .executeUpdate()
-//                    .getKey();
-//        }
-//    }
-//
-//    public static Client find(int id) {
-//        try(Connection con = DB.sql2o.open()) {
-//            String sql = "SELECT * FROM clients where id=:id";
-//            Client client= con.createQuery(sql)
-//                    .addParameter("id", id)
-//                    .executeAndFetchFirst(Client.class);
-//            return client;
-//        }
-//    }
-//
-//    @Override
-//    public boolean equals(Object otherClient){
-//        if (!(otherClient instanceof Client)) {
-//            return false;
-//        } else {
-//            Client newClient = (Client) otherClient;
-//            return this.email.equals(((Client) otherClient).email) &&
-//                    this.getId() == newClient.id;
-//        }
-//    }
+    public static List<Client> all() {
+        String sql = "SELECT * FROM clients";
+        try(Connection con = DB.sql2o.open()) {
+            return con.createQuery(sql).executeAndFetch(Client.class);
+        }
+    }
+
+    public void save() {
+        try(Connection con = DB.sql2o.open())  {
+            String sql = "INSERT INTO clients (name, phone, gender, email, stylist_id) VALUES (:name, :phone, :gender, :email, :stylist_id)";
+            this.id = (int) con.createQuery(sql, true)
+                    .addParameter("name", this.name)
+                    .addParameter("phone",this.phone)
+                    .addParameter("gender", this.gender)
+                    .addParameter("email", this.email)
+                    .addParameter("stylist_id", this.stylist_id)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
+
+    public static Client find(int id) {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "SELECT * FROM clients where id=:id";
+            Client client= con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(Client.class);
+            return client;
+        }
+    }
+
+    @Override
+    public boolean equals(Object otherClient){
+        if (!(otherClient instanceof Client)) {
+            return false;
+        } else {
+            Client newClient = (Client) otherClient;
+            return this.email.equals(((Client) otherClient).email) &&
+                    this.getId() == newClient.id;
+        }
+    }
 
 }
